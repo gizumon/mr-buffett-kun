66ec483dc6194da7d0ab5bb86f163a2ce19203cd
gizumon <a.g0430t.s@gmail.com>
2020-04-01 00:53:10 +0900

    Update error handle

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json(err);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.json(err);
+// });
 
 module.exports = app;
 app.listen(port, () => console.log(`Listening on port ${port}`));
