ec6433530d6619edab98b15fc72d97eb27b84868
gizumon <a.g0430t.s@gmail.com>
2020-04-12 19:18:59 +0900

    Updated

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,7 @@
 const Chatbot = require('../chatbot/chatbot');
 const chatbot = new Chatbot();
 const conf = require('config');
+const _ = require('underscore');
 
 const express = require('express');
 const router = express.Router();
